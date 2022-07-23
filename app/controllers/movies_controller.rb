1793b3e70b3d1f921c766f5896b88cfa548ced1f
Irene Njeru <irenenjeru8@gmail.com>
2022-07-23 21:46:46 +0300

    task completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,14 @@
 class MoviesController < ApplicationController
   wrap_parameters false
+  rescue_from ActiveRecord::RecordInvalid, with: :error_handling
   
   def index
     movies = Movie.all
-    render json: movies
+    render json: movies, status: :ok
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
   end
 
@@ -16,5 +17,8 @@ class MoviesController < ApplicationController
   def movie_params
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
   end
-  
+
+  def error_handling(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
